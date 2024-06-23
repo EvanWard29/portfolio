2fc45e35187fd7cf8d80d7f78875151030f74ec3
Evan Ward <Evan29Ward@gmail.com>
2024-06-23 15:24:27 +0100

    Projects are now displayed dynamically using Blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('portfolio');
+    return view('portfolio', ['projects' => Project::all()]);
 });
