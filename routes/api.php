6e700d5c5e097535a0b685c006c7a3f4b58832d9
Evan Ward <Evan29Ward@gmail.com>
2024-06-23 19:14:30 +0100

    Added basic API routes for CRUD projects

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix' => 'projects'], function () {
+    // List projects
+    Route::get('', function () {
+        return Project::all();
+    });
+
+    // Create a project
+    Route::post('', function () {
+        return Project::create(request()->input());
+    });
+
+    // Update a project
+    Route::put('{project_id}', function (int $projectId) {
+        $project = Project::findOrFail($projectId);
+
+        $project->update(request()->input());
+
+        return $project;
+    });
+
+    // Delete a project
+    Route::delete('{project_id}', function (int $projectId) {
+        Project::findOrFail($projectId)->delete();
+
+        return response()->json();
+    });
+});
