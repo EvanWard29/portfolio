63d05a2186c06a1f333e1a17a9af32bbe9a0ff12
Evan Ward <Evan29Ward@gmail.com>
2024-07-02 17:55:11 +0100

    Updated `project` page to remove `thumbnail_url` and usage of `old()`

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -18,27 +18,23 @@
                 @method('PUT')
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $project->title }}">
+                    <input type="text" class="form-control" id="title" name="title" value="{{ $project->title }}">
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <textarea type="text" class="form-control" id="description" name="description">{{ old('description') ?? $project->description }}</textarea>
+                    <textarea type="text" class="form-control" id="description" name="description">{{ $project->description }}</textarea>
                 </div>
                 <div class="mb-3">
                     <label for="languages" class="form-label">Languages</label>
                     <div id="languages">
-                        @foreach(old('languages') ?? $project->languages as $language)
+                        @foreach($project->languages as $language)
                             <input type="text" value="{{ $language }}" name="languages[]" class="form-control mb-3">
                         @endforeach
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="link" class="form-label">Link</label>
-                    <input type="url" name="link" id="link" class="form-control" value="{{ old('link') ?? $project->link }}">
-                </div>
-                <div class="mb-3">
-                    <label for="thumbnail_url" class="form-label">Link</label>
-                    <input type="text" name="thumbnail_url" id="thumbnail_url" class="form-control" value="{{ old('thumbnail_url') ?? $project->thumbnail_url }}">
+                    <input type="url" name="link" id="link" class="form-control" value="{{ $project->link }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
