79bbf18c61f1d26f0ff38421594a9ba6a2d07595
Evan Ward <Evan29Ward@gmail.com>
2024-07-05 18:17:21 +0100

    Added function for added additional `languages` & `links`

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -31,10 +31,22 @@
                             <input type="text" value="{{ $language }}" name="languages[]" class="form-control mb-3">
                         @endforeach
                     </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" onclick="addField('languages')">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
                 </div>
                 <div class="mb-3">
-                    <label for="link" class="form-label">Link</label>
-                    <input type="url" name="link" id="link" class="form-control" value="{{ $project->link }}">
+                    <label for="links" class="form-label">Links</label>
+                    <div id="links">
+                        @foreach($project->links as $link)
+                            <input type="text" name="links[]" class="form-control mb-3" value="{{ $link }}">
+                        @endforeach
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" onclick="addField('links')">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
@@ -50,6 +62,18 @@
         function OnInput() {
             this.style.height = 'auto';
             this.style.height = (this.scrollHeight) + "px";
+        }
+    </script>
+    <script>
+        function addField(name) {
+            const form = document.getElementById(name)
+            const field = document.createElement('input');
+
+            field.type = 'text';
+            field.name = name + '[]';
+            field.classList.add('form-control', 'mb-3');
+
+            form.appendChild(field);
         }
     </script>
 </html>
