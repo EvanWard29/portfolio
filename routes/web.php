5cc4cd45a227c69d93b9f78b10d7802b0c38961e
Evan Ward <Evan29Ward@gmail.com>
2024-07-09 18:36:19 +0100

    Added a table for languages

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Middleware\OnceBasicAuthMiddleware;
+use App\Models\Language;
 use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'portfolio', ['projects' => Project::all()]);
+Route::view('/', 'portfolio', ['projects' => Project::all(), 'languages' => Language::all()]);
 Route::get('/projects/{project_id}', function (string $projectId) {
     $project = Project::findOrFail($projectId);
 
