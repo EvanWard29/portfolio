05fcff33f1b57613578883daca3450036a07a4fa
Evan Ward <Evan29Ward@gmail.com>
2024-07-11 13:39:58 +0100

    Projects and Skills are now cached

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,21 @@
 use App\Http\Middleware\OnceBasicAuthMiddleware;
 use App\Models\Project;
 use App\Models\Skill;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'portfolio', ['projects' => Project::all(), 'skills' => Skill::all()]);
+Route::get('/', function () {
+    $projects = Cache::rememberForever('projects', function () {
+        return Project::all();
+    });
+
+    $skills = Cache::rememberForever('skills', function () {
+        return Skill::all();
+    });
+
+    return view('portfolio', ['projects' => $projects, 'skills' => $skills]);
+});
+
 Route::get('/projects/{project_id}', function (string $projectId) {
     $project = Project::findOrFail($projectId);
 
