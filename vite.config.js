2437a60ed76c44c29d8d806b4ddd0683edfc49f1
Evan Ward <Evan29Ward@gmail.com>
2024-07-11 15:25:45 +0100

    Added `purgecss` laravel plugin for removing unused CSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
+        }),
+        purge({
+            templates: ['blade'],
+            paths: ['resources/views/*']
         }),
     ],
     server: {
