0c48cce31670abaff5af8f1e2b94567068d0024a
Evan Ward <Evan29Ward@gmail.com>
2024-07-12 13:34:45 +0100

    Update telescope to log everything in production

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -19,16 +19,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 
         $this->hideSensitiveRequestDetails();
 
-        $isLocal = $this->app->environment('local');
-
-        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
-            return $isLocal ||
-                   $entry->isReportableException() ||
-                   $entry->isFailedRequest() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag() ||
-                   $entry->isRequest();
+        Telescope::filter(function (IncomingEntry $entry) {
+            return true;
         });
     }
 
